535cf6bf01d39f512a19c1b74ae52bb391aeb9bd
Matesz063 <30635731+RainbowKingBH@users.noreply.github.com>
2019-05-21 15:01:32 +0200

    Update yellorsten.lua

--- Big Reactors API/yellorsten.lua
+++ Big Reactors API/yellorsten.lua
@@ -1,19 +1,23 @@
 local component = require("component")
 local colors = require("colors")
 local gpu = component.gpu
+term = require("term")
 r = component.br_reactor
-name = Yellorsten
-version = 1.5.4.2
+name = "Yellorsten"
+version = "1.5.4.2"
 rrod = 0
 term.clear()
 os.execute("resolution 45 15")
-io.write("Resolution updated!\n")
+io.write("Resolution updated! \n")
 os.sleep(1)
 term.clear()
-io.write(gpu.setForeground(colors.yellow).. name); io.write(gpu.setForeground(colors.white).. version)
-io.write("Powered by EMDK Launcher\n")
-io.write("launcher.diehard.pl\n")
-os.sleep(1)
+gpu.setForeground(colors.yellow)
+io.write(name.." ")
+gpu.setForeground(colors.black)
+io.write(version.. "\n")
+io.write("Powered by EMDK Launcher ")
+io.write("launcher.diehard.pl \n")
+os.sleep(2)
 term.clear()
 while true do
   rstatus = r.getActive()
@@ -25,28 +29,33 @@ while true do
   rfuelm = r.getFuelAmountMax()
   rfuelc = r.getFuelConsumedLastTick()
   rrod = restored / 100000
+  rfuelcx = rfuelc * 1000
+  rfuelcw = math.ceil(rfuelcx) / 1000
   r.setAllControlRodLevels(rrod)
   os.sleep(0.1)
   term.clear()
-  io.write(gpu.setForeground(colors.yellow).. name); io.write(gpu.setForeground(colors.white).. version)
+  gpu.setForeground(colors.yellow)
+  io.write(name.." ")
+  gpu.setForeground(colors.black)
+  io.write(version.. "\n \n")
   if rstatus == true then
-    gpu.setForeground(colors.white)
-    io.write("Status: \n");
-    gpu.setForeground(colors.green)
-    io.write("Online\n")
+    gpu.setForeground(colors.black)
+    io.write("Status: ")
+    gpu.setForeground(colors.lime)
+    io.write("Online \n")
   end
   if rstatus == false then
-    gpu.setForeground(colors.white)
-    io.write("Status: \n")
+    gpu.setForeground(colors.black)
+    io.write("Status: ")
     gpu.setForeground(colors.red)
-    io.write("Offline\n")
+    iaewweo.write("Offline \n")
   end
-  io.write("Energy produced: "..repoduced"\n")
-  io.write("Energy stored: "..restored.."/10000000 RF\n")
-  io.write("Rods limit: "..rrod.." %\n")
-  io.write("Fuel temperature: "..rtempf.." *C\n")
-  io.write("Case temperature: "..rtempc.." *C\n")
-  io.write("Fuel level: "..rfuel.."/"..rfuelm.." mB\n")
-  io.write("Fuel consume: "..rfuelc.." mB/t\n")
-  io.write("API by Matesz063 and 3Rr0rExE404\n")
+  io.write("Energy produced: "..math.ceil(repoduced).." RF/t \n")
+  io.write("Energy stored: "..restored.."/10000000 RF \n")
+  io.write("Rods limit: "..rrod.." % \n")
+  io.write("Fuel temperature: "..math.ceil(rtempf).." *C \n")
+  io.write("Case temperature: "..math.ceil(rtempc).." *C \n")
+  io.write("Fuel level: "..rfuel.."/"..rfuelm.." mB \n")
+  io.write("Fuel consume: "..rfuelcw.." mB/t \n \n")
+  io.write("API by Matesz063 and 3Rr0rExE404 \n")
 end
